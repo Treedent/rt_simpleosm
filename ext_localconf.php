e889bf664302aeddb5e13f3a6f3e097b96c97080
Maik Schneider <m.schneider@blueways.de>
2022-01-14 16:23:28 +0100

    feat: plugin registration for typo3 v11

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,15 +4,19 @@ defined('TYPO3_MODE') || die('Access denied.');
 call_user_func(
     function() {
 
+        // configure plugin
+        $typo3Version = TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionStringToArray(TYPO3\CMS\Core\Utility\VersionNumberUtility::getNumericTypo3Version());
+        $controllerName = $typo3Version['version_main'] >= 10 ? \SYRADEV\RtSimpleosm\Controller\OsmController::class : 'Osm';
+        $extensionName = $typo3Version['version_main'] >= 10 ? 'RtSimpleosm' : 'SYRADEV.RtSimpleosm';
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-            'SYRADEV.RtSimpleosm',
+            $extensionName,
             'Sosm',
             [
-                'Osm' => 'displayMap'
+                $controllerName => 'displayMap'
             ],
             // non-cacheable actions
             [
-                'Osm' => ''
+                $controllerName => ''
             ]
         );
 
